759ab768392cb7c69b504066630242cd946a54e0
Biplob Talukdar <biplob.codes@gmail.com>
2025-07-03 11:44:33 +0600

    Fix: add request interceptor to dynamically set auth token

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -2,7 +2,12 @@ import axios from "axios";
 export const apiClient = axios.create({
   baseURL: "http://localhost:5000/api/v1",
   withCredentials: true,
-  headers: {
-    Authorization: localStorage.getItem("xpressTalkAccessToken"),
-  },
+});
+
+apiClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("xpressTalkAccessToken");
+  if (token) {
+    config.headers.Authorization = token;
+  }
+  return config;
 });
