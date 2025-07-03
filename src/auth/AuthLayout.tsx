72cc061db60cc0650231734600f8a0b88a017e45
Biplob Talukdar <biplob.codes@gmail.com>
2025-07-04 00:38:16 +0600

    Show welcome phrase on the signup and signin page

--- src/auth/AuthLayout.tsx
+++ src/auth/AuthLayout.tsx
@@ -3,8 +3,11 @@ import { Outlet } from "react-router-dom";
 const AuthLayout = () => {
   return (
     <div className="grid grid-cols-2">
-      <div className="bg-gradient-to-br from-blue-50 to-indigo-100  h-screen">
-        Common Text
+      <div className="bg-gradient-to-br from-blue-50 to-indigo-100 flex justify-center flex-col text-center  h-screen">
+        <h1 className="text-gray-600 text-2xl px-10">
+          Experience messaging that's fast, reliable, and designed to keep your
+          conversations flowing naturally
+        </h1>
       </div>
       <div>
         <Outlet />
